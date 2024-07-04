12fe796dc2f358eb43134b14eb13c389205efb5c
wesleyp846 <wesleyp846@gmail.com>
2024-07-04 06:47:15 -0300

    First Repository of Topico

---
+++ src/main/java/com/forum/ForumHub/repository/TopicosRepository.java
@@ -0,0 +1,9 @@
+package com.forum.ForumHub.repository;
+
+import com.forum.ForumHub.entity.TopicosEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TopicosRepository extends JpaRepository<TopicosEntity, Long> {
+
+
+}
