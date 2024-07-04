8c302b3b0365e0148cc2cde4c708489dc85c6685
wesleyp846 <wesleyp846@gmail.com>
2024-07-03 21:29:37 -0300

    initial commit

---
+++ src/test/java/com/forum/ForumHub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forum.ForumHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
