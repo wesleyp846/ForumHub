851519f5b4738fad39f3f592d9d78c1c780fee0f
wesleyp846 <wesleyp846@gmail.com>
2024-07-06 17:58:17 -0300

    update dto topico

--- src/main/java/com/forum/ForumHub/domain/topico/dto/DetalhaTopicoDTO.java
+++ src/main/java/com/forum/ForumHub/domain/topico/dto/DetalhaTopicoDTO.java
@@ -1,10 +1,12 @@
 package com.forum.ForumHub.domain.topico.dto;
 
+import com.forum.ForumHub.domain.resposta.entity.EntityResposta;
 import com.forum.ForumHub.domain.topico.entity.CursoEnum;
 import com.forum.ForumHub.domain.topico.entity.EstadoDoTopicoEnum;
 import com.forum.ForumHub.domain.topico.entity.TopicosEntity;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record DetalhaTopicoDTO(
 
@@ -14,11 +16,12 @@ public record DetalhaTopicoDTO(
         String mensagem,
         LocalDateTime dataCriacao,
         CursoEnum curso,
-        EstadoDoTopicoEnum status) {
+        EstadoDoTopicoEnum status,
+        List<EntityResposta> reposta) {
 
     public DetalhaTopicoDTO(TopicosEntity topico) {
         this(topico.getId(), topico.getUsuario().getNome(), topico.getTitulo(),
                 topico.getMensagem(), topico.getDataCriacao(), topico.getCurso(),
-                topico.getStatus());
+                topico.getStatus(), topico.getRespostas());
     }
 }
