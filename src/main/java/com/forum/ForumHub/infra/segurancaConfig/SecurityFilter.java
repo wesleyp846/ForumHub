ba327ee1f759b2cec758562a5f0cd5249e13571a
wesleyp846 <wesleyp846@gmail.com>
2024-07-08 14:44:17 -0300

    Preparando filtro de autenticados

---
+++ src/main/java/com/forum/ForumHub/infra/segurancaConfig/SecurityFilter.java
@@ -0,0 +1,50 @@
+package com.forum.ForumHub.infra.segurancaConfig;
+
+import com.forum.ForumHub.domain.auth.AdmRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private AdmRepository repository;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+//        var tokenJWT = recuperarToken(request);
+//
+//        if (tokenJWT != null){
+//            var subject = tokenService.getSubject(tokenJWT);
+//            var usuario = repository.findByLogin(subject);
+//
+//            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+//
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+//        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var autorizationHeader = request.getHeader("Authorization");
+        if (autorizationHeader != null){
+            return autorizationHeader.replace("Bearer ", "");
+        }
+        return null;
+    }
+}
